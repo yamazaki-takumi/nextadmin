b5f2942b79a14e327666392e74ce5cfedc6e74d4
yamazaki_takumi <yamazaki_takumi@mdv.co.jp>
2024-03-04 13:11:01 +0900

    flexの文字が間違っていたので、修正しました。

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -3,17 +3,17 @@ import Sidebar from "../ui/dashboard/sidebar/sidebar";
 import styles from "../ui/dashboard/dashboard.module.css";
 
 const Layout = ({ children }) => {
-  return (
-    <div className={styles.container}>
-      <div classname={styles.menu}>
-        <Sidebar />
-      </div>
-      <div className={styles.content}>
-        <Navbar />
-        {children}
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<div classname={styles.menu}>
+				<Sidebar />
+			</div>
+			<div className={styles.content}>
+				<Navbar />
+				{children}
+			</div>
+		</div>
+	);
 };
 
 export default Layout;
