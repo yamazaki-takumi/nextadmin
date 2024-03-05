3461cb6261eca545fd2d4d4c707ec1fedc292841
Takumi <jfw501st.3ya.mybride@gmail.com>
2024-03-06 00:08:53 +0900

    navbarを作成、sidebarにlogoutボタンを追加

--- app/ui/dashboard/sidebar/menuLink/menuLink.jsx
+++ app/ui/dashboard/sidebar/menuLink/menuLink.jsx
@@ -4,21 +4,19 @@ import styles from "./menuLink.module.css";
 import { usePathname } from "next/navigation";
 
 const MenuLink = ({ item }) => {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	console.log(pathname);
-
-	return (
-		<Link
-			href={item.path}
-			className={`${styles.container} ${
-				pathname === item.path && styles.active
-			}`}
-		>
-			{item.icon}
-			{item.title}
-		</Link>
-	);
+  return (
+    <Link
+      href={item.path}
+      className={`${styles.container} ${
+        pathname === item.path && styles.active
+      }`}
+    >
+      {item.icon}
+      {item.title}
+    </Link>
+  );
 };
 
 export default MenuLink;
