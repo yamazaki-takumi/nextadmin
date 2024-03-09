97bb7474e2332415ebfe926c8fa50063d5f73e50
Takumi <jfw501st.3ya.mybride@gmail.com>
2024-03-09 21:39:45 +0900

    Chartコンポーネントを作成しました。

--- app/ui/dashboard/chart/chart.jsx
+++ app/ui/dashboard/chart/chart.jsx
@@ -1,7 +1,75 @@
+"use client";
+
+import {
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import styles from "./chart.module.css";
 
+const data = [
+  {
+    name: "Sun",
+    visit: 4000,
+    click: 2400,
+  },
+  {
+    name: "Mon",
+    visit: 3000,
+    click: 1300,
+  },
+  {
+    name: "Tue",
+    visit: 2000,
+    click: 3000,
+  },
+  {
+    name: "Wed",
+    visit: 2700,
+    click: 3908,
+  },
+  {
+    name: "Thu",
+    visit: 1800,
+    click: 4800,
+  },
+  {
+    name: "Fri",
+    visit: 2390,
+    click: 3800,
+  },
+  {
+    name: "Sat",
+    visit: 3400,
+    click: 4300,
+  },
+];
+
 const Chart = () => {
-	return <div>Chart</div>;
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.title}>Weekly Recap</h2>
+      <ResponsiveContainer width="100%" height="90%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        >
+          <XAxis dataKey="name" />
+          <YAxis />
+          <Tooltip contentStyle={{ background: "#151c2c", border: "none" }} />
+          <Legend />
+          <Line type="monotone" dataKey="visit" stroke="#8884d8" />
+          <Line type="monotone" dataKey="click" stroke="#82ca9d" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Chart;
